12a2ddb857e2f6a031ff2934f639d78e659e9f4e
Laggrif <ugo.novello@gmail.com>
2022-11-04 20:51:05 +0100

    Basics done

--- Server.py
+++ Server.py
@@ -1,55 +1,91 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_restful import Api
+import multiprocessing
+import os
+import threading
+
+from flask import Flask, render_template, url_for, send_file, jsonify
+from flask_restful import Api, Resource
+from flask_login import LoginManager
 from waitress import serve
+import Discord.src.Discord_Bot_Laggrif.Discord_Bot as dBot
+import Discord.src.Discord_Bot_Laggrif.cogs.Display as dDisplay
+import cv2
+
+path = os.path.dirname(os.path.realpath(__file__))
+
 
 app = Flask(__name__)
 api = Api(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+bot = dBot.get_bot()
+
+
+def start_bot(which):
+    process = multiprocessing.Process(target=dBot.run, args=(which, ))
+    process.start()
+    return process
+
+
+process = start_bot('Test')
+
+
+connected_time = 0
+
+display = False
 
 
 @app.route('/')
 def home():
-    return render_template('Home.html')
-
-
-@app.route('/lights')
-def lights():
-    return render_template('Lights.html')
-
-
-@app.route('/lights', methods=['POST', 'GET'])
-def lights_post():
-    red = request.form['Red']
-    return render_template(url_for('lights', red=red))
-
-
-@app.route('/discord', methods=['POST', 'GET'])
-def discord():
-    return render_template('Discord.html', display_state='On')
+    return render_template('home.html', connect_time='0')
 
 
 @app.route('/discord')
-def reboot():
-    print('restarted')
-    return redirect(url_for("discord"))
+def discord():
+    return render_template('discord.html')
 
 
-@app.route('/display')
-def display():
-    return redirect(url_for("discord"))
+class AddTime(Resource):
+    def get(self):
+        global connected_time
+        connected_time += 1
+        return connected_time
 
 
-@app.route('/display-mode', methods=['GET', 'POST'])
-def display_mode():
-    mode = request.values['select-display']
-    return redirect(url_for("discord"))
+class SubTime(Resource):
+    def get(self):
+        global connected_time
+        connected_time -= 1
+        return connected_time
 
 
-@app.route('/reload-api', methods=['GET', 'POST'])
-def reload_api():
-    print(request.form)
-    api = request.form['API_KEY']
-    print(api)
-    return redirect(url_for("discord"))
+class Webcam(Resource):
+    def get(self):
+        # TODO get cam img
+        url = path + '/static/webcam0.jpeg'
+        return send_file(url)
+
+
+class DiscordRestart(Resource):
+    def get(self):
+        global process
+        process.kill()
+        process = start_bot('Test')
+
+
+class DiscordDisplay(Resource):
+    def get(self):
+        global display
+        display = not display
+        return 'enabled' if display else 'disabled'
+
+
+api.add_resource(AddTime, '/api/time/add')
+api.add_resource(SubTime, '/api/time/sub')
+api.add_resource(Webcam, '/api/webcam')
+api.add_resource(DiscordRestart, '/api/discord/restart')
+api.add_resource(DiscordDisplay, '/api/discord/display')
 
 
 if __name__ == '__main__':
--- static/discord.js
+++ static/discord.js
@@ -1,11 +1,44 @@
-const api_but = document.getElementById("change-api");
-
-function reload_api() {
-    $.ajax({
-        api_but.style.color = '#000000'
-    })
+function notification(message, doneFunc = null) {
+    $('#notification-text').text(message);
+    let notif = $('#notification');
+    notif.toggleClass('hide', false);
+    notif.toggleClass('show', true);
+    setTimeout(function() {
+        notif.toggleClass('show', false);
+        notif.toggleClass('hide', true);
+    if (doneFunc != null) {
+        doneFunc();
+    }
+    }, 1500);
 }
 
-api_but.addEventListener('click', function onClick() {
-    reload_api();
-});
+$(document).ready(function () {
+
+    $('#restart').click(function () {
+        notification('Restarted');
+        $.ajax({
+            url: '/api/discord/restart',
+            success: function (data) {
+                $('#restart').attr('disabled', true);
+                setTimeout(function () {
+                    $('#restart').attr('disabled', false);
+                }, 1000);
+            }
+        });
+    });
+
+
+    $('#display').click(function () {
+        $.ajax({
+            url: '/api/discord/display',
+            success: function (data) {
+
+                let checkbox = $('#display');
+                checkbox.prop('disabled', true);
+                notification('Display is ' + data, function () {
+                    checkbox.prop('disabled', false);
+                });
+            }
+        });
+    });
+})
